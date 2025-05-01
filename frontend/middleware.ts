2ff451a2eecf90cca6ff94bd1af7d71bfc44a0b8
Waqar Hussain <waqar105lgu.edu@gmail.com>
2025-05-01 14:42:50 +0500

    add axios instance for docker

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import axiosInstance from './services/axios';
+import { axiosDockerInstance } from './services/axios';
 import { UserRoleEnum } from './types/user.type';
 import { PAGES_ROUTES } from './constants/routes.constants';
 import { ENDPOINT } from './constants/api.constants';
@@ -8,32 +8,27 @@ const publicPaths = [PAGES_ROUTES.login, PAGES_ROUTES.signup];
 const adminPaths = [PAGES_ROUTES.emailTemplate];
 
 export async function middleware(request: NextRequest) {
-
   const path = request.nextUrl.pathname;
 
-  const isPublicPath = publicPaths.some(publicPath =>
-    path === publicPath || path.startsWith(`${publicPath}/`)
+  const isPublicPath = publicPaths.some(
+    publicPath => path === publicPath || path.startsWith(`${publicPath}/`),
   );
-
-
 
   const accessToken = request.cookies.get('accessToken')?.value;
 
   if (isPublicPath && accessToken) {
-    return NextResponse.redirect(new URL(PAGES_ROUTES.careerDashboard, request.url));
+    return NextResponse.redirect(
+      new URL(PAGES_ROUTES.careerDashboard, request.url),
+    );
   }
 
   if (isPublicPath) {
     return NextResponse.next();
   }
   //check current path is admin path
-  const isAdminPath = adminPaths.some(adminPath =>
-    path === adminPath || path.startsWith(`${adminPath}/`)
+  const isAdminPath = adminPaths.some(
+    adminPath => path === adminPath || path.startsWith(`${adminPath}/`),
   );
-
-
-
-
 
   if (!accessToken) {
     const loginUrl = new URL(PAGES_ROUTES.login, request.url);
@@ -42,31 +37,37 @@ export async function middleware(request: NextRequest) {
   }
 
   try {
-
-    const validateToken = await axiosInstance.get(ENDPOINT.validateToken, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
+    const validateToken = await axiosDockerInstance.get(
+      ENDPOINT.validateToken,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
 
     if (!validateToken.status) {
-      const response = NextResponse.redirect(new URL(PAGES_ROUTES.login, request.url));
+      const response = NextResponse.redirect(
+        new URL(PAGES_ROUTES.login, request.url),
+      );
       response.cookies.delete('accessToken');
       return response;
     }
 
-
     const userRole = validateToken.data.user.role;
 
     if (isAdminPath && userRole !== UserRoleEnum.SUPER_ADMIN) {
-      return NextResponse.redirect(new URL(PAGES_ROUTES.careerDashboard, request.url));
+      return NextResponse.redirect(
+        new URL(PAGES_ROUTES.careerDashboard, request.url),
+      );
     }
 
     return NextResponse.next();
-
   } catch (error) {
     console.error('Error validating token:', error);
-    const response = NextResponse.redirect(new URL(PAGES_ROUTES.login, request.url));
+    const response = NextResponse.redirect(
+      new URL(PAGES_ROUTES.login, request.url),
+    );
     response.cookies.delete('accessToken');
     return response;
   }
--- frontend/services/axios.ts
+++ frontend/services/axios.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://rise_backend:5000',
-  // baseURL: process.env.NEXT_PUBLIC_BACKEND_URI,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URI,
+  headers: {
+    'Content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  },
+});
+
+export const axiosDockerInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URI_DOCKER,
   headers: {
     'Content-type': 'application/json',
     'Access-Control-Allow-Origin': '*',
